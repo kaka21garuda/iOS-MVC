5857d0caac56a140443e88f9c99575a9fd98247d
Buka Cakrawala <kaka10garuda@gmail.com>
2016-11-14 11:55:01 -0800

    Computable Protocol Challenge completed

--- 1114-m/protocols-challenge/Protocols.playground/Pages/Computable.xcplaygroundpage/Contents.swift
+++ 1114-m/protocols-challenge/Protocols.playground/Pages/Computable.xcplaygroundpage/Contents.swift
@@ -6,20 +6,42 @@ protocol Computable {
     func compute() -> Int
 }
 
-class Sum {
-  var numbers = [1,2,3,4,5,6,7,8,9]
+class Sum: Computable {
+    var numbers = [1,2,3,4,5,6,7,8,9]
+    var result = 0
+    
+    func compute() -> Int {
+        for number in numbers {
+            result += number
+        }
+        return result
+    }
 }
 
-class Product {
-  var numbers = [1,2,3,4,5,6,7,8,9]
+class Product: Computable {
+    var numbers = [1,2,3,4,5,6,7,8,9]
+    var result = 1
+    
+    func compute() -> Int {
+        for number in numbers{
+           result *= number
+        }
+        return result
+    }
 }
 
-class CountCharacters {
-  var string: String = "odinfeoivneflndwcnervbunlinwe;dqwmid"
+class CountCharacters: Computable {
+    var string: String = "odinfeoivneflndwcnervbunlinwe;dqwmid"
+    
+    func compute() -> Int {
+        return string.characters.count
+    }
 }
 
-class Random {
-  
+class Random: Computable {
+    func compute() -> Int {
+        return Int(arc4random())
+    }
 }
 
 let sum = Sum()
@@ -27,11 +49,14 @@ let product = Product()
 let countCharacters = CountCharacters()
 let random = Random()
 
-//let arrayOfComputables = [sum, product, countCharacters, random]
+let arrayOfComputables: [Computable] = [sum, product, countCharacters, random]
 
 /*:
- **Tasks**: 
+ **Tasks**:
  1. Uncomment above line and make the code compile (by implementing the `Computable` protocol in all the classes)
  2. Iterate over `arrayOfComputables`, _compute_ each result and print it to the console using `print`
  */
 
+for computableArray in arrayOfComputables {
+    print(computableArray.compute())
+}
