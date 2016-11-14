4d8e2973dc72d2ac585ebe4401bb18deabb1478e
Buka Cakrawala <kaka10garuda@gmail.com>
2016-11-14 11:25:11 -0800

    canMakeNoise Challenge Completed

---
+++ 1114-m/protocols-challenge/Protocols.playground/Pages/CanMakeNoise.xcplaygroundpage/Contents.swift
@@ -0,0 +1,41 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+protocol CanMakeNoise {
+  func makeNoise()
+}
+
+class Human: CanMakeNoise {
+    func makeNoise() {
+        print("Hello world!")
+    }
+}
+
+class Pig: CanMakeNoise {
+    func makeNoise() {
+        print("ONNGG!")
+    }
+}
+
+class Cow: CanMakeNoise {
+    func makeNoise() {
+        print("MOOOO!")
+    }
+}
+
+let human = Human()
+let pig = Pig()
+let cow = Cow()
+
+let arrayOfNoiseMaker: [CanMakeNoise] = [human, pig, cow]
+
+/*:
+ **Tasks**:
+ 1. Uncomment above line and make the code compile. This can be achieved by implementing the `CanMakeNoise` protocol in all the classes. Think about a noise each class could make and print it to the console using `print`.
+ 2. Iterate over `arrayOfNoiseMaker` and let each object make their noise
+ */
+
+for eachNoiseMaker in arrayOfNoiseMaker {
+    eachNoiseMaker.makeNoise()
+}
